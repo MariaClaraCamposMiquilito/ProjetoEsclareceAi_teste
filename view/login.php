cd5ea4d9c160ba48c6d7787709dc6657e70134f9
Maria Clara Miquelito <miquelitomaria@gmail.com>
2025-03-10 16:49:08 -0300

    10/2/2025

--- view/login.php
+++ view/login.php
@@ -9,25 +9,59 @@
         <title> Login </title>
         <link rel="icon" href="http://localhost/ProjetoEsclareceAi_teste/assets/logoSemFundo.png"/>
         <link rel="stylesheet" href="http://localhost/ProjetoEsclareceAi_teste/styles/styles.css">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     </head>
 
     <body> 
-        asdfgh
         <main class = "apresentacao">
 
-        laaalaaalallalallalalla
-
             <div class = "campo1">
-                <img src="http://localhost/ProjetoEsclareceAi_teste/assets/logoCompletaSemFundo.png" width="300" height="300">
+                <div class = "campo_imagem">
+                    <img src="http://localhost/ProjetoEsclareceAi_teste/assets/logoCompletaSemFundo.png" width="200" height="200">        
+                </div>
+
+                <div class = "campo_texto">
+                    <p class = "slogan"> O Lugar Perfeito Para Estudantes Esclarecerem Suas Dúvidas</p>
+                </div>
             </div>
 
             <div class = "campo2">
-                <forms>
-                    <input class = "campo_login" type="text" placeholder="E-mail"><br><br>
-                    <input class = "campo_login" type="password" placeholder="Senha"><br><br>
-                    <a class ="botao" href="http://localhost/ProjetoEsclareceAi_teste/principal.php"> Entrar </a><br>
-                    <a class="Registro" href="http://localhost/ProjetoEsclareceAi_teste/cadastro.php"> Cadastrar-se</a>
+
+                <div class = "campo_titulo">
+                    <p class = "titulo_login">Login</p>
+                    <p class = "subtitulo_login">Preencha os campos com seus dados de acesso</p>
+                </div>
+
+                <forms class = "campo_login">
+                    <div class = "campo_login_icone">
+                        <span class="input-group-text">
+                            <i class="bi bi-person"></i>
+                        </span>
+                        <input class = "campo_login_input" type="text" placeholder="E-mail">
+                    </div>
+
+                    <div class = "campo_login_icone">
+                        <span class="input-group-text">
+                            <i class="bi bi-lock"></i>
+                        </span>
+                        <input class = "campo_login_input" type="password" placeholder="Senha">
+                    </div>
+
+                    <a class = "esqueceu_senha" href="#">Esqueci minha senha</a>
+                    
+                    <div class = "campo_login_botoes">
+                        <button class ="botao" href="http://localhost/ProjetoEsclareceAi_teste/View/principal.php">Acessar</button>
+                        
+                        <div class = "botao_login_google">
+                            <img src="http://localhost/ProjetoEsclareceAi_teste/assets/icone_google.png" width="25" height="20">        
+                            <a class = "login_google" href ="#">Fazer login com o Google</a>
+                        </div>
+                        
+                        <a class="registro" href="http://localhost/ProjetoEsclareceAi_teste/View/cadastro.php">Criar uma conta</a>
+                    </div>
+                    
                 </forms>
+
             </div>
         </main>
 
